1567f656d7e85888681a7328853ca46598b3c1ff
Huan Kang <huan.kang@bomoda.com>
2014-02-11 09:32:43 -0500

    FirstAssignment

---
+++ src/mahJong/MahJongLogic.java
@@ -0,0 +1,14 @@
+package mahJong;
+
+
+import org.cheat.client.GameApi.VerifyMove;
+import org.cheat.client.GameApi.VerifyMoveDone;
+
+public class MahJongLogic {
+	
+	 public VerifyMoveDone verify(VerifyMove verifyMove) {
+		    // TODO: I will implement this method in HW2
+		    return new VerifyMoveDone();
+		  }
+
+}
